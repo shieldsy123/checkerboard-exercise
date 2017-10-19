c43359ff40b43a291a119f5c0229fbef7cbf44be
shieldsy123 <michael.t.shields@gmail.com>
2017-10-19 15:50:59 -0600

    Checkerboard: Black & Red

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// Your JS goes here
+for (var i = 0; i < 90; i++){
+  var div = document.createElement("div");
+  div.style.float = "left";
+  div.style.width = "11.1%";
+  div.style.height = "100px";
+  if ((i%2) == 0) {
+    div.style.background = "red";
+  } else {
+    div.style.background = "black";
+  }
+  document.body.appendChild(div);
+}
