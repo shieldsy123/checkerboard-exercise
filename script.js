a0abc844aac953387249fcac6497172a8c7e0744
shieldsy123 <michael.t.shields@gmail.com>
2017-10-19 16:11:36 -0600

    Checkerboard: Flashing

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-for (var i = 0; i < 90; i++){
+for (i = 0; i < 90; i++){
   var div = document.createElement("div");
   div.style.float = "left";
   div.style.width = "11.1%";
   div.style.height = "100px";
-  if ((i%2) == 0) {
-    div.style.background = "linear-gradient(to top, orange, white)";
-  } else {
-    div.style.background = "linear-gradient(to bottom, orange, white)";
-  }
+  div.style.background = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
   document.body.appendChild(div);
 }
+
+setInterval(function(){
+  for (i = 0; i < 90; i++){
+    var div = document.getElementsByTagName("div")[i];
+    div.style.background = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+  }
+}, 1000);
